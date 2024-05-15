7afacf94510550f6238c8c58213bb5a0422684e4
umanrai <umanrai58@gmail.com>
2024-05-16 00:17:39 +0545

    ui changes for auth_base and base layout

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -33,6 +33,7 @@ class ProductController extends AbstractController
             $entityManager->persist($product);
             $entityManager->flush();
 
+            $this->addFlash('success', 'Product saved successfully!');
             return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
         }
 
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,6 +6,8 @@ use App\Entity\Category;
 use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,14 +16,24 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price')
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
+                'attr' => ['class' => 'form-control'],
             ])
-        ;
+            ->add('name', null, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('price', NumberType::class, [
+                'attr' => ['class' => 'form-control', 'type' => 'number'],
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'rows' => '6',
+                    'placeholder' => 'Enter the description'
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
